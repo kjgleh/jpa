cf8d0feef21db27f0bd10827dd6a3be840294cd3
김정근 <gogoogjg@tripstore.kr>
2024-07-24 08:47:57 +0900

    ..

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,12 +21,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
     runtimeOnly("mysql:mysql-connector-java")
-    testImplementation("org.springframework.boot:spring-boot-starter-test") {
-        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-    }
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("com.appmattus.fixture:fixture:0.7.1")
 }
 
